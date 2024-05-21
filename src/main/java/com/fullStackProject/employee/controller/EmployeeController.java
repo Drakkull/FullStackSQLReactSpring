83272e33432151d273278e8549d0789c5dde98ec
Leonardo DiAbreu <drakkull007@hotmail.com>
2024-05-21 09:04:58 -0300

    Backend Done!

--- src/main/java/com/fullStackProject/employee/controller/EmployeeController.java
+++ src/main/java/com/fullStackProject/employee/controller/EmployeeController.java
@@ -3,8 +3,13 @@ package com.fullStackProject.employee.controller;
 
 import com.fullStackProject.employee.entity.Employee;
 import com.fullStackProject.employee.service.EmployeeService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -19,6 +24,45 @@ public class EmployeeController {
     public Employee postEmployee(@RequestBody Employee employee){
         return employeeService.postEmployee(employee);
 
+    }
+
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+
+        return employeeService.getAllEmployees();
+    }
+
+
+    @DeleteMapping("/employee/{id}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable Long id){
+
+        try{
+            employeeService.deleteEmployee(id);
+            return new ResponseEntity<>("Employee with ID  " + id + " FOI DE RALO", HttpStatus.OK);
+        }catch(EntityNotFoundException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+
+    @GetMapping("/employee/{id}")
+    public ResponseEntity<?> getEmployeeById(@PathVariable Long id){
+
+        Employee employee = employeeService.getEmployeeById(id);
+        if(employee == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(employee);
+
+    }
+
+    @PatchMapping("/employee/{id}")
+    public ResponseEntity<?> updateEmployee(@PathVariable Long id, @RequestBody Employee employee){
+        Employee updatedEmployee = employeeService.updateEmployee(id, employee);
+
+
+        if(updatedEmployee == null) return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        return ResponseEntity.ok(updatedEmployee);
     }
 
 
--- src/main/java/com/fullStackProject/employee/service/EmployeeService.java
+++ src/main/java/com/fullStackProject/employee/service/EmployeeService.java
@@ -1,10 +1,15 @@
 package com.fullStackProject.employee.service;
 
 
+import com.fullStackProject.employee.EmployeeApplication;
 import com.fullStackProject.employee.entity.Employee;
 import com.fullStackProject.employee.repository.EmployeeRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +19,45 @@ public class EmployeeService {
 
     public Employee postEmployee(Employee employee){
         return employeeRepository.save(employee);
+    }
+
+
+    public List<Employee> getAllEmployees(){
+
+        return employeeRepository.findAll();
+
+    }
+
+    public void deleteEmployee (Long id){
+         if(!employeeRepository.existsById(id)){
+             throw new EntityNotFoundException("Employee with ID " + id + " not FOUND. ENCONTRE O CORRETO, MISÉRIA!");
+
+         }
+        employeeRepository.deleteById(id);
+    }
+
+
+    public Employee getEmployeeById(Long id){
+        return employeeRepository.findById(id).orElse(null);
+
+    }
+
+    public Employee updateEmployee(Long id, Employee employee){
+
+        Optional<Employee> optionalEmployee = employeeRepository.findById(id);
+        if(optionalEmployee.isPresent()){
+          Employee existingEmployee = optionalEmployee.get();
+          existingEmployee.setEmail(employee.getEmail());
+          existingEmployee.setName(employee.getName());
+          existingEmployee.setPhone(employee.getPhone());
+          existingEmployee.setDepartment(employee.getDepartment());
+
+
+          return employeeRepository.save(existingEmployee);
+
+        }
+        return null;
+
     }
 
 }
